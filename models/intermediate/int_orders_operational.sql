e9e773178447f13c437cdea09854aed80affad69
Yann Bouclet <yannbouclet@gmail.com>
2025-07-31 12:26:09 +0000

    same

--- models/intermediate/int_orders_operational.sql
+++ models/intermediate/int_orders_operational.sql
@@ -8,4 +8,4 @@ SELECT
   m.margin + s.shipping_fee - s.logcost - CAST(s.ship_cost AS FLOAT64) AS operational_margin
 FROM {{ ref('int_orders_margin') }} AS m
 LEFT JOIN {{ ref('stg_raw__ship') }} AS s
-  ON m.orders_id = s.order_id
+  ON m.orders_id = s.orders_id
