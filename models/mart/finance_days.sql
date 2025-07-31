651f7c1424e6532270a07605618161bcfe01b96a
Yann Bouclet <yannbouclet@gmail.com>
2025-07-31 12:43:22 +0000

    Finance team report

---
+++ models/mart/finance_days.sql
@@ -0,0 +1,18 @@
+SELECT
+    m.date_date,
+    COUNT(m.orders_id) AS nb_transactions,
+    ROUND(SUM(m.revenue), 2) AS total_revenue,
+    ROUND(SUM(m.revenue) / COUNT(m.orders_id), 2) AS avg_basket,
+    ROUND(SUM(o.operational_margin), 2) AS operational_margin,
+    ROUND(SUM(m.purchase_cost), 2) AS total_purchase_cost,
+    ROUND(SUM(s.shipping_fee), 2) AS total_shipping_fee,
+    ROUND(SUM(s.logcost), 2) AS total_logcost,
+    SUM(m.quantity) AS total_qty
+    
+FROM {{ref("int_orders_margin")}} AS m
+JOIN {{ref("int_orders_operational")}} AS o
+    USING(orders_id)
+JOIN {{ref("stg_raw__ship")}} AS s
+    USING(orders_id)
+GROUP BY m.date_date
+ORDER BY m.date_date DESC
